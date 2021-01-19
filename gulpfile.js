1f3ece7ad55662402dad202611f15a0a5f0903c5
Jesus Camacaro <jesus.camacaro@imageimaker.com>
2021-01-19 16:01:54 -0300

    ambiente local

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 
 gulp.task('default', () => gulp.src('src/**/*.js')
   .pipe(babel({
     presets: ['@babel/env']
   }))
+  .pipe(concat('index.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('dist')));
---
+++ index.js
@@ -0,0 +1,5 @@
+import { Logger } from './src';
+
+const log = new Logger('index');
+
+log.info('Hola Mundo');
